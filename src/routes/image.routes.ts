eee32f2262bba171db8cab4a6bb2eac35fdb3e6e
Aditya Kumar Rai <rai.aditya2k@gmail.com>
2022-12-20 22:29:05 +0530

    routes settign

--- src/routes/image.routes.ts
+++ src/routes/image.routes.ts
@@ -6,11 +6,13 @@ import {
 } from "../middleware/multer.middleware";
 const router: Router = Router();
 
-const { imageWithoutPasswordController, imageWithPasswordController } =
+const { imageWithoutPasswordController, imageWithPasswordController,decryptBase64WithPassword,decryptBase64 } =
   new ImageController();
 
 router.post("/", multerSingleUpload, imageWithoutPasswordController);
 router.post("/password", multerMultiUpload, imageWithPasswordController);
+router.post("/decryptPass",multerMultiUpload, decryptBase64WithPassword);
+router.post("/decrypt",multerMultiUpload, decryptBase64);
 
 export default router;
 
